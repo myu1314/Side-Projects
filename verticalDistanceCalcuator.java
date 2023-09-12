4046b51045968f9b4dbc6cf87721e81fd24f9ece
myu1314 <142926759+myu1314@users.noreply.github.com>
2023-09-12 09:27:43 -0700

    Update verticalDistanceCalcuator.java

--- verticalDistanceCalcuator.java
+++ verticalDistanceCalcuator.java
@@ -19,13 +19,14 @@ public class verticalDistanceCalcuator {
         double t = keyboard.nextDouble();
         System.out.print("What is the acceleration due to gravity? (m/s^2) ");
         double g = keyboard.nextDouble();
-        System.out.print(g);
-        double yf = 1/2 * g * t * t + vi * t + yi;
-        System.out.print("The max height is " + yf);
+        double yf = 1/2.0f * g * t * t + vi * t + yi;
+        System.out.printf("The max height is %4.2f", yf);
+        System.out.printf(" 0G: %.2f\n t: %.2f\nyi: %.2f\nvi: %.2f\n", g+t, t, yi, vi);
+
      }
 }
 /*
-1/2at + vit + y = yf
+1/2at^2 + vit + y = yf
  * ****************************OUTPUT**************************************
  * What is your first name and last name? walter savitch
 * Your name in Pig Latin is: Alterway Avitchsay
