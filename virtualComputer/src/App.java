9ddcfa92633bb1ef6725ee962dbf405b52310480
myu1314 <142926759+myu1314@users.noreply.github.com>
2024-01-06 16:56:34 -0800

    Added Edits to the virtual Computer

--- virtualComputer/src/App.java
+++ virtualComputer/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Or firstGateOr= new Or();
+        Or firstGateOr = new Or();
 
         System.out.println(firstGateOr.getInA);
     }
